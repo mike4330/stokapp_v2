0cdd29c599c944afceb87e025d09bc9dc6d76726
mike4330 <mike@roetto.org>
2025-06-20 18:36:46 -0400

    Add react-helmet-async for improved document head management: Integrate HelmetProvider in the main application entry point to enable dynamic management of the document head, enhancing SEO and accessibility features.

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -7,6 +7,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { HelmetProvider } from 'react-helmet-async';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -16,7 +17,9 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
   </React.StrictMode>
 );
 
